ee6a38bffcc638bad36ae2d761e5eacb453bd194
Lorodo <barrioleal1994@gmail.com>
2020-06-10 22:25:39 +0200

    el random sigue sin estar claro

--- testjson.py
+++ testjson.py
@@ -5,11 +5,15 @@ with open('selenium_py/configuracion.json','r') as fichero_config:
     config = fichero_config.read()
 
 configuracion = json.loads(config)
-
+print(configuracion['Mensajes'])
 mensajes = configuracion['Mensajes'].values()
-print(mensajes)
+print(mensajes.values())
 
 for mensaje in mensajes:
     print(mensaje)
 
-random.choice(dict(mensajes.values()))
+
+movie_list = ['The Godfather', 'The Wizard of Oz', 'Citizen Kane', 'The Shawshank Redemption', 'Pulp Fiction']
+
+moview_item = random.choice(movie_list)
+print ("Randomly selected item from list is - ", moview_item)
