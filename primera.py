24e5b33057af76c3bfce2bcc3c4d8b3f95e9f734
Lorodo <barrioleal1994@gmail.com>
2020-06-16 12:12:37 +0200

    el import

--- primera.py
+++ primera.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 import json
 import time
+import random
 
 
 #recupero fichero de configuracion
